afa1b0a5b7df70091bd0e217f0018f472f024b07
rlaalsrl715 <minkik715@gmail.com>
2023-02-06 20:54:36 +0900

    fix: 403 error Proxy 로 해결

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/PuerkitoBio/goquery"
 	"log"
 	"net/http"
+	"net/url"
 )
 
 var baseURL string = "https://kr.indeed.com/jobs?q=python&limit=50"
@@ -15,11 +16,28 @@ func main() {
 }
 
 func getPages() int {
-	res, err := http.Get(baseURL)
+	res := getHttp(baseURL)
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+	doc.Find("./pagination").Each()
+	fmt.Println(doc)
+
+	return 0
+}
+
+func getHttp(sUrl string) *http.Response {
+	fmt.Println("[GET]Request :", sUrl)
+	req, rErr := http.NewRequest("GET", sUrl, nil)
+	checkErr(rErr)
+	req.Header.Add("User-Agent", `Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.27 Safari/537.36`)
+
+	purl, err := url.Parse(sUrl)
+	client := &http.Client{Transport: &http.Transport{Proxy: http.ProxyURL(purl)}}
+	res, err := client.Do(req)
 	checkErr(err)
 	checkResCode(res)
-	goquery.ToEnd
-	return 0
+
+	return res
 }
 
 func checkErr(err error) {
